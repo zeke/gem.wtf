13c2a31c298f4037acca68e26e9421c1f0771b57
Zeke Sikelianos <zeke@sikelianos.com>
2021-01-22 22:54:08 -0800

    redirect http to https

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express()
 const wtf = require('./wtf')
 const port = Number(process.env.PORT) || 3000
 
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] !== 'https') {
+    res.redirect('https://gem.wtf' + req.url)
+  } else {
+    next()
+  }
+})
+
 app.get('/', (req, res) => {
   res.redirect('https://github.com/zeke/gem.wtf')
 })
